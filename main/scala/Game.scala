2c6b13b14d216ec8bb5f6c3719415d833dcb3f57
Vincent Herreros <vincent-herreros@hotmail.fr>
2018-10-02 13:35:30 +0200

    start display

--- main/scala/Game.scala
+++ main/scala/Game.scala
@@ -1,9 +1,24 @@
 object Game extends App{
 	def mainloop(){
-		var input = readLine("choisir nom")
-		var b = new Boat(3, List(List(3,24), List(2,1)))
+		val input = readLine("choisir nom")
+		val b = new Boat(3, List(List(3,24), List(2,1)))
 		println(b.isInTheGrid(20, b.listPos))
+
 
 	}
 	mainloop()
+
+
+	def display(colums: Int, lines: Int){
+
+	}
+
+	def displayColumns(colums: Int){
+		if(colums==0){
+			
+		}
+	}
+
+	def displayLines()
+
 }
