5bb1a7a3ebae2c87ddf578738dbcfbc0e0fda4e1
Vincent Herreros <vincent-herreros@hotmail.fr>
2018-10-05 17:06:06 +0200

    display work

--- main/scala/Game.scala
+++ main/scala/Game.scala
@@ -1,49 +1,66 @@
 object Game extends App{
 
 	val list1 = List(List(1,2), List(1,3), List(1,4))
-		val list2 = List(List(5,4), List(6,4), List(7,4))
-		val b1 = Boat(3, list1)
-		val b2 = Boat(3, list2)
-		println("Choose your game mode")
-		println("Player VS Player : 1")
-		println("Player VS AI : 2")
-		println("AI VS AI")
-		println("Quit : q")
-		val inputMenu = readLine("")
-		inputMenu match{
-			case "1" =>	{
-				println("Nom du premier joueur : ")
-				val input1 = readLine()
-				println("Nom du deuxième joueur : ")
-				val input2 = readLine()
-				val p1 = new Player(input1, List(), List())
-				val p2 = new Player(input1, List(), List())
-				mainloop(p1, p2) 
-			}
-			case "2" =>	{
-
-			}
-			case "3" => {
-
-			}
-			case _ => {
-			}
+	val list2 = List(List(5,4), List(6,4), List(7,4))
+	val b1 = Boat(3, list1)
+	val b2 = Boat(3, list2)
+	println("Choose your game mode")
+	println("Player VS Player : 1")
+	println("Player VS AI : 2")
+	println("AI VS AI")
+	println("Quit : q")
+	val inputMenu = readLine("")
+	inputMenu match{
+		case "1" =>	{
+			println("Nom du premier joueur : ")
+			val input1 = readLine()
+			println("Nom du deuxième joueur : ")
+			val input2 = readLine()
+			val p1 = new Player(input1, List(), List())
+			val p2 = new Player(input2, List(), List())
+			mainloop(p1, p2, p1, 0, p2, 0)
 		}
+		case "2" =>	{
 
-	def mainloop(p1: Player, p2: Player): Unit= {
-		val newp1 = new Player(p1.name, p1.addFleetToPlayer(List(), 2))
-		val newp2 = new Player(p2.name, p2.addFleetToPlayer(List(), 2))
+		}
+		case "3" => {
 
-		gameloop(newp1, newp2)
+		}
+		case _ => {
+		}
 	}
 
-	def gameloop(p1: Player, p2: Player): Unit = {
-		if(p1.fleet.isEmpty){
-			println(p1.name+" loose")
+	def mainloop(p1: Player, p2: Player, player1: Player, score1: Int, player2: Player, score2: Int): Unit= {
+		val newp1 = new Player(p1.name, p1.addFleetToPlayer(List(b1, b2), 0), List(new Hit(List(2, 2), true), new Hit(List(3, 2), false)))
+		val newp2 = new Player(p2.name, p2.addFleetToPlayer(List(b1), 0))
+		val winner = gameloop(newp1, newp2)
+		if(winner == newp1.name){
+			val newScore1 = score1+1
+			println(player1.name + " : "+newScore1+ "     "+player2.name+" : "+score2)
+			val input = readLine("Play again ?")
+			if(input=="y"){
+				mainloop(newp2, newp1, newp2, score2,newp1, newScore1)
+			}
 		}
 		else{
-			displayLines(10, 10, p1.fleet.map(x => (x.life, x.listPos.flatMap(x => x))).collect{ case (x, y) => y}.flatMap(x => x), List(), false)
-			displayLines(10, 10, List(), p1.shoots, true)
+			val newScore2 = score2+1
+			println(player1.name + " : "+score1+ "     "+player2.name+" : "+newScore2)
+			val input = readLine("Play again ?")
+			if(input=="y"){
+				mainloop(newp2, newp1, newp2, newScore2,newp1, score1)
+			}
+		}
+
+	}
+
+	def gameloop(p1: Player, p2: Player): String = {
+		if(p1.fleet.isEmpty){
+			println(p1.name+" loose")
+			p2.name
+		}
+		else{
+			println("grille de tir de "+p1.name)
+			displayLines(10, 10, p1.fleet.map(x => (x.life, x.listPos.flatMap(x => x))).collect{ case (x, y) => y}.flatMap(x => x), p1.shoots)
 			val shootx = readLine().toInt
 			val shooty = readLine().toInt
 			val newp2 = p1.shoot(List(shootx,shooty), p2)
@@ -82,7 +99,7 @@ object Game extends App{
 							displayColumns(colums-1, List(),hits.tail:::nextHits, List(), hitOrBoat)
 						}
 						else{
-							displayColumns(colums, List(), hits.tail, List(a), hitOrBoat)
+							displayColumns(colums, List(), hits.tail, List(a):::nextHits, hitOrBoat)
 						}
 					}
 				}
@@ -102,24 +119,19 @@ object Game extends App{
 		}
 	}
 
-	def displayLines(colums: Int, lines: Int, posBoat:List[Int] = List(), hits: List[Hit] = List(), hitOrBoat:Boolean): Unit = {
+	def displayLines(colums: Int, lines: Int, posBoat:List[Int] = List(), hits: List[Hit] = List()): Unit = {
 		if(lines==10){
 			println(" _ _ _ _ _ _ _ _ _ _")
 		}
 		if (lines>0){
-			if(hitOrBoat){
-				val l1 = hits.collect{case x if x.pos(1)==11-lines => x}
-				displayColumns(colums, List(), l1, List(), hitOrBoat)
-				println("")
-				displayLines(colums, lines-1, List() ,hits, hitOrBoat)
-			}
-			else{
-				val l1 = posBoat.zipWithIndex.collect{ case ( x, i) if (i%2==1 && x==11-lines) => i}
-				val l2 = posBoat.zipWithIndex.collect{ case ( x, i) if l1.contains(i+1) => x}
-				displayColumns(colums, l2, List(), List(),hitOrBoat)
-				println("")
-				displayLines(colums, lines-1, posBoat, List(), hitOrBoat)
-			}
+			val l1 = hits.collect{case x if x.pos(1)==11-lines => x}
+			displayColumns(colums, List(), l1, List(), true)
+			print("         ")
+			val l3 = posBoat.zipWithIndex.collect{ case ( x, i) if (i%2==1 && x==11-lines) => i}
+			val l2 = posBoat.zipWithIndex.collect{ case ( x, i) if l3.contains(i+1) => x}
+			displayColumns(colums, l2, List(), List(),false)
+			println("")
+			displayLines(colums, lines-1, posBoat, hits)
 		}
 		else{
 			println("")
